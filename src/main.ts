e70701006823f18692ec685109a3028c12089f79
Yousef Al-waer <elwaeryousef@gmail.com>
2023-11-05 16:14:00 +0200

    enable cors and use global pipeline

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // enable cors
+  app.enableCors({
+    origin: '*',
+  });
+
+  // use validation pipe
+  app.useGlobalPipes(new ValidationPipe());
+  // set global prefix
+  app.setGlobalPrefix('api/v1');
+
+
   await app.listen(3000);
 }
 bootstrap();
