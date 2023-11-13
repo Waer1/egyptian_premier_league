0f7dce5e7cde0255145ba25ce78c682e8db3578d
AhmedHosny2024 <eng.ahmedhosny2024@gmail.com>
2023-11-13 18:44:54 +0200

    feat : create react app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import ResponsiveAppBar from './Components/nav';
+import Order from './Components/Order/Order';
+import { Container } from './style';
+import Home from './Components/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveAppBar state={3}/>
+      <Container>
+        <Order/>
+        <Home/>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
