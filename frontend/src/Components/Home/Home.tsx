7d1b716531eadeefdb53b5eea69f92c4b99d5bdb
AhmedHosny2024 <eng.ahmedhosny2024@gmail.com>
2023-11-13 18:45:26 +0200

    feat : add navbar and teams order

---
+++ frontend/src/Components/Home/Home.tsx
@@ -0,0 +1,16 @@
+import { Container } from "./style";
+
+export default function Home() {
+
+    return (
+        <Container>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repellat sit blanditiis in repellendus dolor facere est laboriosam quibusdam esse cupiditate quod vel voluptatem, quaerat dignissimos architecto minus animi vitae!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repellat sit blanditiis in repellendus dolor facere est laboriosam quibusdam esse cupiditate quod vel voluptatem, quaerat dignissimos architecto minus animi vitae!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repellat sit blanditiis in repellendus dolor facere est laboriosam quibusdam esse cupiditate quod vel voluptatem, quaerat dignissimos architecto minus animi vitae!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repellat sit blanditiis in repellendus dolor facere est laboriosam quibusdam esse cupiditate quod vel voluptatem, quaerat dignissimos architecto minus animi vitae!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repellat sit blanditiis in repellendus dolor facere est laboriosam quibusdam esse cupiditate quod vel voluptatem, quaerat dignissimos architecto minus animi vitae!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repellat sit blanditiis in repellendus dolor facere est laboriosam quibusdam esse cupiditate quod vel voluptatem, quaerat dignissimos architecto minus animi vitae!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repellat sit blanditiis in repellendus dolor facere est laboriosam quibusdam esse cupiditate quod vel voluptatem, quaerat dignissimos architecto minus animi vitae!
+        </Container>
+    );
+}
---
+++ frontend/src/Components/Home/style.tsx
@@ -0,0 +1,9 @@
+import { Box, styled } from "@mui/material";
+
+export const Container = styled(Box)(() => ({
+    // marginTop:15,
+    height:"100%",
+    marginLeft:10,
+    paddingLeft:10,
+    borderLeft:"1px solid #e0e0e0",
+    }));
