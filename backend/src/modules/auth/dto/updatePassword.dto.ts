14b01088f6ddd5958e64b96129af98b04592a0bd
Waer1 <elwaeryousef@gmail.com>
2023-12-21 23:01:01 +0200

    add update password to swagger

--- backend/src/modules/auth/dto/updatePassword.dto.ts
+++ backend/src/modules/auth/dto/updatePassword.dto.ts
@@ -1,9 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, MinLength } from 'class-validator';
 
 export class UpdatePasswordDto {
+  @ApiProperty({
+    description: 'The old password',
+    required: true,
+  })
   @IsNotEmpty()
   oldPassword: string;
 
+  @ApiProperty({
+    description: 'The new password',
+    minLength: 8,
+    required: true,
+  })
   @IsNotEmpty()
   @MinLength(8, {
     message: 'Password is too short, minimum length is 8 characters',
